c079c8937f7eacbe6c5c5ddb3e2bd621e0dbd5ca
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-10-22 19:01:45 -0300

    EacEnvs::Http::Rspec::EchoServer.on_aquired_port: use random port.

--- lib/eac_envs/http/rspec/echo_server.rb
+++ lib/eac_envs/http/rspec/echo_server.rb
@@ -2,6 +2,7 @@
 
 require 'addressable'
 require 'eac_ruby_utils/core_ext'
+require 'random-port'
 require 'webrick'
 require 'webrick/https'
 
@@ -11,8 +12,8 @@ module EacEnvs
       class EchoServer
         HOSTNAME = 'localhost'
         SCHEMES = {
-          http: { Port: 8080 },
-          https: { Port: 8443, SSLEnable: true, SSLCertName: [['CN', HOSTNAME]] }
+          http: {},
+          https: { SSLEnable: true, SSLCertName: [['CN', HOSTNAME]] }
         }.freeze
 
         class << self
@@ -27,22 +28,39 @@ module EacEnvs
         common_constructor :scheme, :webrick_options
 
         def on_active(&block)
-          http_server.on_running(&block)
+          on_aquired_port do
+            http_server.on_running(&block)
+          end
         end
 
         def root_url
           ::Addressable::URI.new(
             scheme: scheme.to_s,
             host: ::EacEnvs::Http::Rspec::EchoServer::HOSTNAME,
-            port: webrick_options.fetch(:Port)
+            port: port!
           )
         end
 
         protected
 
+        attr_accessor :port
+
         # @return [EacEnvs::Http::Rspec::EchoServer::HttpServer]
         def http_server
-          ::EacEnvs::Http::Rspec::EchoServer::HttpServer.new(webrick_options)
+          ::EacEnvs::Http::Rspec::EchoServer::HttpServer.new(webrick_options.merge(Port: port!))
+        end
+
+        def on_aquired_port(&block)
+          ::RandomPort::Pool.new.acquire do |port|
+            self.port = port
+            block.call
+            self.port = nil
+          end
+        end
+
+        # @return [Integer]
+        def port!
+          port.presence || raise('Port unsetted')
         end
 
         require_sub __FILE__
